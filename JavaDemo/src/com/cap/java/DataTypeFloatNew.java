51224a296080fffd8936bde5a4a7c52c3ab0d1c6
Adhiraj Deshmukh <Adhiraj Deshmukh@DESKTOP-4SMPERH>
2022-10-19 13:02:36 +0530

    first commit

---
+++ JavaDemo/src/com/cap/java/DataTypeFloatNew.java
@@ -0,0 +1,16 @@
+package com.cap.java;
+
+public class DataTypeFloatNew {
+
+	public static void main(String[] args) 
+	{
+		boolean var = false;
+		System.out.println("var="+var);
+
+		char var1 = '\u00A7';
+		System.out.println("var1="+var1);
+
+
+	}
+
+}
---
+++ JavaDemo/src/com/cap/java/StringA.java
@@ -0,0 +1,14 @@
+package com.cap.java;
+
+
+public class StringA {
+
+	public static void main(String[] args)
+	{
+		String var = new String("Hello world");
+		System.out.println(var);
+		// TODO Auto-generated method stub
+
+	}
+
+}
