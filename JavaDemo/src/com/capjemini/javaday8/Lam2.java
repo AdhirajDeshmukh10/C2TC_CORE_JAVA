829fdab6f984cdf7194bcb745ff531c4d2ade135
Adhiraj Deshmukh <Adhiraj Deshmukh@DESKTOP-4SMPERH>
2022-11-25 12:53:37 +0530

    first commit

--- JavaDemo/src/com/capjemini/javaday8/Lam2.java
+++ JavaDemo/src/com/capjemini/javaday8/Lam2.java
@@ -5,12 +5,13 @@ interface Lambda1
 }
 
 
-public class Lam2 {
+public class Lam2
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		
 		 new Thread ( ()->{ System.out.println("This is my thread1 ..running");}).start();
-
-}
+	}
 
 }
